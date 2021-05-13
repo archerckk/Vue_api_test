51a4cb7c1619888ceb0fdc251a3e70ffa9154138
archerckk@163.com <archerckk@163.com>
2021-05-13 09:37:34 +0800

    新增部分练习4代码

--- Vue_api_test/Myapp/views.py
+++ Vue_api_test/Myapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse,HttpResponse
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+
+value_list=['apple','pear','banana']
+def search_key(request):
+    method=request.method
+    if method=='POST':
+        key=request.body['key']
+
+        for i in value_list:
+            if key in value_list:
+                return key
+            else:
+                return HttpResponse(status=404)
